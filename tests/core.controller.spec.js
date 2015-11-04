5ea5d5ecec36cee95e95f199b3cb23bb5c67458e
Clay Shaw <Claydshaw@gmail.com>
2015-11-04 14:54:02 -0800

    initial testing start

--- tests/core.controller.spec.js
+++ tests/core.controller.spec.js
@@ -6,18 +6,25 @@ describe('testing the CoreController', function() {
 		// load app module
 		module('naut');
 		// inject controller
-		inject(function ($controller, $rootScope) {
-			scope = $rootScope.$new();
-			CoreController = $controller('CoreController', {
-				$scope: scope
-			});
+		inject(function (_$controller_, $rootScope) {
+			scope = $rootScope;
+			$controller = _$controller_;
 		});
 	});
 
+	describe('$rootScope.pageTitle function', function() {
+	    it('sets the application name and description', function() {
+            var $rootScope = {};
+            var controller = $controller('CoreController', { $rootScope : $rootScope });
+            // setting up values for function
+            $rootScope.app = {name:'foo_app', description:'foo_description'}; 
+            $rootScope.pageTitle();
+            expect($rootScope.pageTitle()).toEqual('foo_app - foo_description');
+        });
 
-	it('am i crazy', function() {
-		var check = true;
-		expect(check).toEqual(true);
-	});
+	
+
+    });
+
 
 });
