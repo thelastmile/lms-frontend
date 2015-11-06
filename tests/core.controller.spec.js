602273db0616aacae5b734991cfc317a6079d88e
Clay Shaw <Claydshaw@gmail.com>
2015-11-06 10:20:57 -0800

    complete controller tests

--- tests/core.controller.spec.js
+++ tests/core.controller.spec.js
@@ -1,30 +1,52 @@
 // Suite
-describe('testing the CoreController', function() {
-	var $controller, scope;
-
-	beforeEach(function(){	
-		// load app module
+describe('testing the CoreController: ', function() {
+	var $controller, scope, UserService;
+	// setup for each spec
+	beforeEach(function() {	
+		// load app module and any services
 		module('naut');
-		// inject controller
-		inject(function (_$controller_, $rootScope) {
-			scope = $rootScope;
+		module(function($provide) {
+			$provide.service('UserService', function() {
+				this.logout = jasmine.createSpy('logout')
+			});
+		});
+		// inject services
+		inject(function (_$controller_, _$rootScope_, _UserService_) {
 			$controller = _$controller_;
+			$rootScope = _$rootScope_;
+			UserService = _UserService_;
 		});
 	});
-
-	describe('$rootScope.pageTitle function', function() {
-	    it('sets the application name and description', function() {
-            var $rootScope = {};
-            var controller = $controller('CoreController', { $rootScope : $rootScope });
-            // setting up values for function
-            $rootScope.app = {name:'foo_app', description:'foo_description'}; 
-            $rootScope.pageTitle();
-            expect($rootScope.pageTitle()).toEqual('foo_app - foo_description');
-        });
-
-	
-
+	// specs for the 3 functions
+    it('sets the application name and description', function() {
+    	var $rootScope = {};
+        var controller = $controller('CoreController', { $rootScope : $rootScope });
+        // setting up values for function
+        $rootScope.app = {name:'foo_app', description:'foo_description'}; 
+        $rootScope.pageTitle();
+        expect($rootScope.pageTitle()).toEqual('foo_app - foo_description');
     });
 
+	it('$rootScope.cancel - runs two methods.', function(done) {
+		var $rootScope = {};
+		var mockEvent = {
+			preventDefault: jasmine.createSpy('preventDefault'),
+			stopPropagation: jasmine.createSpy('stopPropagation'),
+		}
+		var controller = $controller('CoreController', { $rootScope : $rootScope });
+		// setting up values for function
+		$rootScope.cancel(mockEvent);
+		expect(mockEvent.preventDefault).toHaveBeenCalled();
+		expect(mockEvent.stopPropagation).toHaveBeenCalled();
+		done();
+	});
+
+	it('$rootScope.logout - function that logs out of UserService', function(done) {
+		var controller = $controller('CoreController', { $rootScope : $rootScope });
+		// function we are testing
+		$rootScope.logout();
+		expect(UserService.logout).toHaveBeenCalled();
+		done();
+	});
 
 });
