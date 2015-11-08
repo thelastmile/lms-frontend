7c2d4db4c75cd6dd730b8a4d6b88d05dac5fa2e8
SomeKittens <rkoutnik@gmail.com>
2015-11-08 12:06:43 -0800

    students can edit from note view page

--- src/js/modules/notes/notes-service.js
+++ src/js/modules/notes/notes-service.js
@@ -5,22 +5,34 @@
 
   notes.$inject = ['$http', '$window', 'customUrl'];
   function notes($http, $window, customUrl) {
-    var id = Number($window.sessionStorage.getItem('id'));
+
+    var userId = Number($window.sessionStorage.getItem('id'));
     return {
       create: function (noteData) {
         return $http.post(customUrl.url + '/api/note/', {
           title: noteData.title,
           body: noteData.body,
-          object_id: 2,
-          author: id,
-          content_type: 16 // module
+          object_id: 2, // ID of current module
+          author: userId,
+          content_type: 16 // module table
+        });
+      },
+      save: function (noteData) {
+        return $http.put(customUrl.url + '/api/note/' + noteData.id + '/', {
+          title: noteData.title,
+          body: noteData.body,
+          pk: noteData.id,
+          object_id: 2, // ID of current module
+          author: userId,
+          content_type: 16 // module table
         });
       },
       fetchAll: function () {
         return $http.get(customUrl.url + '/api/note/')
         .then(function (response) {
+          // Ideally, this would be filtered on the Django side
           return response.data.filter(function (note) {
-            return note.author === id;
+            return note.author === userId;
           });
         });
       }
--- src/js/modules/notes/studentnotes-controller.js
+++ src/js/modules/notes/studentnotes-controller.js
@@ -12,5 +12,17 @@
     .then(function (notes) {
       SNC.notes = notes;
     });
+
+    // If not editing, enables edit mote
+    // If editing, saves and disables editing
+    SNC.editing = false;
+    SNC.editSave = function () {
+      if (SNC.editing) {
+        SNC.editing = false;
+        notes.save(SNC.selectedNote);
+      } else {
+        SNC.editing = true;
+      }
+    }
   }
 })();
