32e21ca5eda3d0c2c558b210e7aa9a7fef76f38e
Clay Shaw <Claydshaw@gmail.com>
2015-11-10 11:08:42 -0800

    add comments

--- src/js/modules/viewStudents/viewStudents.services.js
+++ src/js/modules/viewStudents/viewStudents.services.js
@@ -13,15 +13,15 @@
     //==============================================
     .service('StudentInfoService', ['customUrl', '$http', function(customUrl, $http){
         var students = {};
-        
+        // create a student
         students.post= function(data){
             return $http.post(customUrl.url + '/api/users/', data);
         };
-        
+        // list of students
         students.get = function(){
             return $http.get(customUrl.url + '/api/users/')
         };
-
+        // find 1 student
         students.get_single = function(pk){
             return $http.get(customUrl.url + '/api/users/' + pk + '/');
         };
