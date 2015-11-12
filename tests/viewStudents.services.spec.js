45bce327eee3c558c2ca34df5c04c46ffd30b665
Clay Shaw <Claydshaw@gmail.com>
2015-11-12 10:38:12 -0800

    wip: updates for backend mock tests

--- tests/viewStudents.services.spec.js
+++ tests/viewStudents.services.spec.js
@@ -1,43 +1,38 @@
-describe('Test StudentInfoService: ', function() {
-	var customUrl, $http;
+describe('StudentInfoService => ', function() {
+
+	var StudentInfoServiceObj, $httpBackend, $rootScope;
 	// load modules
-	beforeEach(function() {
-		module('naut');
-		module(function($provide) {
-			$provide.service('$http', function() {
-				var students = {};
-				students.post = jasmine.createSpy('post');
-				students.get = jasmine.createSpy('get');
-				students.get_single = jasmine.createSpy('get_single');
-				return students;
-			});
-			$provide.service('customUrl', function(){
-				this.url = jasmine.createSpy('url');
-			});
-		});
-		module('StudentInfoService');
-	});
+	beforeEach(function() { module('naut') });
 	// inject service dependencies
-	beforeEach(inject(function($http, customUrl) {
-		mockCustomUrl = customUrl;
-		mockhttp = $http;
+	beforeEach(inject(function(StudentInfoService, $injector, _$controller_) {
+		StudentInfoServiceObj = StudentInfoService;
+		$httpBackend = $injector.get('$httpBackend');
+		$rootScope = $injector.get('$rootScope');
 	}));
 
-	it('should call a POST to a given url with some data:', function() {
-		var data = {'data':'data'};
-		students.post(data);
-		expect(students.post).toHaveBeenCalledWith(data);
+	afterEach(function() {
+	     $httpBackend.verifyNoOutstandingExpectation();
+	     $httpBackend.verifyNoOutstandingRequest();
 	});
 
-	it('should call a GET to a given url:', function() {
-		students.get();
-		expect(students.get).toHaveBeenCalled();
+	// it('should call a POST to a given url with some data:', function() {
+	// 	var data = {'data':'data'};
+	// 	authRequestHandler = $httpBackend.when('GET','/api/users/').respond('...success')
+	// 	$httpBackend.expectGET('/api/users/');
+	// 	$httpBackend.flush();
+	// });
+
+
+	it('should call a GET:', function() {
+		$httpBackend.expectGET('/api/users/');
+		$httpBackend.flush();
+		expect(StudentInfoService.get)
 	});
 
-	it('should call a GET to a given url with pk data:', function() {
-		var student = 1;
-		students.get(student);
-		expect(students.get).toHaveBeenCalledWith(student);
-	});
+	// it('should call a GET to a given url with pk data:', function() {
+	// 	var student = 1;
+	// 	students.get(student);
+	// 	expect(students.get).toHaveBeenCalledWith(student);
+	// });
 
 });
