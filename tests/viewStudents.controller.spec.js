f184437d1964e00e6990e303c5a1bd23879887f1
Clay Shaw <Claydshaw@gmail.com>
2015-11-13 12:08:40 -0800

    initial commit

---
+++ tests/viewStudents.controller.spec.js
@@ -0,0 +1,10 @@
+describe('ViewStudentsController test =>', function() {
+
+	var ViewStudentsControllerObj;
+
+	beforeEach(module('naut'));
+
+	beforeEach(inject(function(_$controller_) {
+		$controller = _$controller_;
+	}));
+});
