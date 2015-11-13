66a2515f13bc566acfe128ea02e1757cbc57e1e8
Clay Shaw <Claydshaw@gmail.com>
2015-11-13 12:09:00 -0800

    wip: testing backend calls

--- tests/viewStudents.services.spec.js
+++ tests/viewStudents.services.spec.js
@@ -1,38 +1,47 @@
-describe('StudentInfoService => ', function() {
+describe('ViewStudentService test =>', function() {
 
-	var StudentInfoServiceObj, $httpBackend, $rootScope;
-	// load modules
-	beforeEach(function() { module('naut') });
-	// inject service dependencies
-	beforeEach(inject(function(StudentInfoService, $injector, _$controller_) {
-		StudentInfoServiceObj = StudentInfoService;
-		$httpBackend = $injector.get('$httpBackend');
-		$rootScope = $injector.get('$rootScope');
-	}));
+  var StudentInfoServiceObj, $httpBackend;
 
-	afterEach(function() {
-	     $httpBackend.verifyNoOutstandingExpectation();
-	     $httpBackend.verifyNoOutstandingRequest();
-	});
-
-	// it('should call a POST to a given url with some data:', function() {
-	// 	var data = {'data':'data'};
-	// 	authRequestHandler = $httpBackend.when('GET','/api/users/').respond('...success')
-	// 	$httpBackend.expectGET('/api/users/');
-	// 	$httpBackend.flush();
-	// });
+  beforeEach(module('naut'));
 
 
-	it('should call a GET:', function() {
-		$httpBackend.expectGET('/api/users/');
-		$httpBackend.flush();
-		expect(StudentInfoService.get)
-	});
+  beforeEach(module('naut', function ($provide, $translateProvider) {
 
-	// it('should call a GET to a given url with pk data:', function() {
-	// 	var student = 1;
-	// 	students.get(student);
-	// 	expect(students.get).toHaveBeenCalledWith(student);
-	// });
+    $provide.factory('customLoader', function () {
+      // loader logic goes here
+    });
+
+    $translateProvider.useLoader('customLoader');
+
+  }));
+
+  beforeEach(inject(function(_$httpBackend_, StudentInfoService){
+    $scope = {};
+    $httpBackend = _$httpBackend_;
+    StudentInfoServiceObj = StudentInfoService;
+  }));
+
+  it('defines StudentInfoServiceObj:', function() {
+    console.log(StudentInfoServiceObj);
+    expect(StudentInfoServiceObj).toBeDefined();
+  });
+
+  it('defines StudentInfoServiceObj post method', function() {
+    expect(StudentInfoServiceObj.post).toBeDefined();
+  });
+
+  xit('checks for a POST call', function() {
+    var data = {name:'foo_user'};
+    $httpBackend.expectPOST('/api/users/', data).respond(201, 'success');
+    StudentInfoServiceObj.post(data);
+    $httpBackend.flush();
+  });
+
+  it('tests for a GET to /api/users/ : ', function() {
+    $httpBackend.expectGET('/api/users/')
+    StudentInfoServiceObj.get('/api/users/');
+    $httpBackend.flush();
+  });
 
 });
+
