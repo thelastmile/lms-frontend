402c85d7ea47c28863f2955e2369c22afa32548b
Clay Shaw <Claydshaw@gmail.com>
2015-11-16 15:59:03 -0800

    complete calls tests and overwrite angular-translate loader

--- tests/viewStudents.services.spec.js
+++ tests/viewStudents.services.spec.js
@@ -2,7 +2,18 @@ describe('ViewStudentService test =>', function() {
 
   var StudentInfoServiceObj, $httpBackend;
 
-  beforeEach(module('naut'));
+  // remove the angular-translate implicit XHR call by overriding.
+  beforeEach(module('naut', function($provide, $translateProvider) {
+    $provide.factory('customLoader', function ($q) {
+        return function () {
+          var deferred = $q.defer();
+          deferred.resolve({});
+          return deferred.promise;
+        };
+      });
+     
+      $translateProvider.useLoader('customLoader');
+  }));
 
   beforeEach(inject(function($injector, StudentInfoService){
     $scope = {};
@@ -18,23 +29,27 @@ describe('ViewStudentService test =>', function() {
     expect(StudentInfoServiceObj.post).toBeDefined();
   });
 
-  xit('checks for a POST call', function() {
+  it('defines StudentInfoServiceObj post method', function() {
+    expect(typeof StudentInfoServiceObj.post).toBe('function');
+  });
+
+  it('checks for a POST call', function() {
     var data = {'user':'test_user'}
-    $httpBackend.expectPOST('', data).respond(201, 'success');
+    $httpBackend.expectPOST('http://127.0.0.1:8000/api/users/', data).respond(201, 'success');
     StudentInfoServiceObj.post(data);
     $httpBackend.flush();
   });
 
   it('tests for a GET to /api/users/ : ', function() {
-    $httpBackend.expectGET('').respond({});
-    $httpBackend.flush();
+    $httpBackend.expectGET('http://127.0.0.1:8000/api/users/').respond({});
     StudentInfoServiceObj.get('/api/users/');
+    $httpBackend.flush();
   });
 
   it('tests for a GET to /api/users/ with a pk:', function() {
-    $httpBackend.expectGET('/api/users/1/').respond({});
+    $httpBackend.expectGET('http://127.0.0.1:8000/api/users/1/').respond({});
+    StudentInfoServiceObj.get_single(1)
     $httpBackend.flush();
-    StudentInfoServiceObj.get('/api/users/1/')
   });
 });
 
