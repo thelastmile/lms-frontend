817db1953661fe065fc00291b8aa826d09b3d566
Clay Shaw <Claydshaw@gmail.com>
2015-11-17 10:37:04 -0800

    initial commit

---
+++ tests/core.config.spec.js
@@ -0,0 +1,27 @@
+describe('CoreConfig Module => ', function() {
+
+	beforeEach(module('naut'));
+
+	// remove the angular-translate implicit XHR call by overriding.
+	beforeEach(module('naut', function($provide, $translateProvider) {
+	  $provide.factory('customLoader', function ($q) {
+	      return function () {
+	        var deferred = $q.defer();
+	        deferred.resolve({});
+	        return deferred.promise;
+	      };
+	    });
+	   
+	    $translateProvider.useLoader('customLoader');
+	}));
+
+	beforeEach(inject(function($injector){
+		$rootScope = injector.get('$rootScope');
+		$state = injector.get('$state');
+		$templateCache = injector.get('$templateCache');
+		$http = injector.get('$http')
+		UserService = injector.get('UserService');
+	}));
+
+	it('')
+});
