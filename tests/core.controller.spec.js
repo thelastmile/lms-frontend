2bc8420389ec453b0b047377c15aa1ab2b10fed6
Ben Havilland <ben@havilland.net>
2015-11-20 10:15:47 -0800

    making notes a modal

--- tests/core.controller.spec.js
+++ tests/core.controller.spec.js
@@ -2,29 +2,39 @@
 describe('testing the CoreController', function() {
 	var $controller, scope;
 
-	beforeEach(function(){	
-		// load app module
-		module('naut');
-		// inject controller
-		inject(function (_$controller_, $rootScope) {
-			scope = $rootScope;
-			$controller = _$controller_;
-		});
+	// beforeEach(function(){	
+	// 	// load app module
+	// 	module('naut');
+	// 	// inject controller were testing
+	// 	inject(function(_$controller_) {
+	// 		$controller = _$controller_;
+	// 	});
+	// });
+
+	beforeEach(inject(function ($rootScope, $controller) {
+			scope = $rootScope.$new();
+			CoreController = $controller('CoreController', {
+				$scope: scope
+			});
+		}));
+
+	it('am i crazy', function() {
+		var check = true;
+		expect(check).toEqual(true);
 	});
 
+	/*
 	describe('$rootScope.pageTitle function', function() {
-	    it('sets the application name and description', function() {
-            var $rootScope = {};
-            var controller = $controller('CoreController', { $rootScope : $rootScope });
-            // setting up values for function
-            $rootScope.app = {name:'foo_app', description:'foo_description'}; 
-            $rootScope.pageTitle();
-            expect($rootScope.pageTitle()).toEqual('foo_app - foo_description');
-        });
+		it('sets the application name and description', function() {
+			var $rootScope = {};
+			var controller = $controller('CoreController', { $rootScope : $rootScope });
+			// setting up values for function
+			$rootScope.app = {name:'foo_app', description:'foo_description'}; 
+			$rootScope.pageTitle();
+			expect($rootScope.pageTitle()).toEqual('foo_app - foo_description');
+		});
 
-	
-
-    });
-
+	});
+	*/
 
 });
