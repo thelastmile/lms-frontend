86f04c0aea4e6e741f7920f30a4722be565e5458
Clay Shaw <Claydshaw@gmail.com>
2015-11-20 13:23:55 -0800

    rebase

--- tests/core.config.spec.js
+++ tests/core.config.spec.js
@@ -34,6 +34,7 @@ describe('CoreConfig Module => ', function() {
 		expect($rootScope.$on).toBeDefined();
 	});
 
+<<<<<<< HEAD
 	it('should redirect if user is logged in', function() {
 		spyOn($state, 'transitionTo')
 		UserService.isAuthenticated = false;
@@ -43,10 +44,41 @@ describe('CoreConfig Module => ', function() {
 		expect($state.transitionTo).toHaveBeenCalled(); // this fails if we .toHaveBeenCalledWith('page.login')
 	});
 
+=======
+
+
+	it('checks $rootScope', function() {
+		expect($rootScope).toBeDefined();
+	});
+
+	it('checks $rootScope.$on', function() {
+		expect($rootScope.$on).toBeDefined();
+	});
+
+	it('should redirect if user is logged in', function() {
+		spyOn($state, 'transitionTo')
+		UserService.isAuthenticated = false;
+		var toState = { authenticate: true };
+		$state.go('app.dashboard');
+		$rootScope.$apply();
+		expect($state.transitionTo).toHaveBeenCalled(); // this fails if we .toHaveBeenCalledWith('page.login')
+	});
+
+>>>>>>> wip: complete login re-route
 	it('customUrl function test for localhost return', function() {
 		var document = {location: {hostname: 'localhost'}};
 		expect(customUrl.url).toBe('http://127.0.0.1:8000')
 	});
+<<<<<<< HEAD
+=======
+
+	it('customUrl function test for aws url return', function() {
+		var customDocument = function() {
+			return document.location.assign('www.google.com');
+		};
+		expect(customUrl.url).toBe('http://lms-backend-dev.elasticbeanstalk.com');
+	})
+>>>>>>> wip: complete login re-route
 
 	it('customUrl function test for aws url return', function() {
 		var customDocument = function() {
