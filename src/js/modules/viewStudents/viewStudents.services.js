ea57893b27ae6a588a5f171f7ddf4663e31fe34f
Ben Havilland <ben@havilland.net>
2015-11-24 13:59:12 -0800

    added limited endpoint for students

--- src/js/modules/viewStudents/viewStudents.services.js
+++ src/js/modules/viewStudents/viewStudents.services.js
@@ -15,15 +15,15 @@
         var students = {};
         // create a student
         students.post= function(data){
-            return $http.post(customUrl.url + '/api/users/', data);
+            return $http.post(customUrl.url + '/api/students/', data);
         };
         // list of students
         students.get = function(){
-            return $http.get(customUrl.url + '/api/users/')
+            return $http.get(customUrl.url + '/api/students/')
         };
         // find 1 student
         students.get_single = function(pk){
-            return $http.get(customUrl.url + '/api/users/' + pk + '/');
+            return $http.get(customUrl.url + '/api/students/' + pk + '/');
         };
         
         return students;
