ffbe9ab01a9883b0d45838175b5650e01c4324f6
Ben Havilland <ben@havilland.net>
2015-11-24 18:49:16 -0800

    merged lesson services

--- src/js/modules/admin/adminLessons.controller.js
+++ src/js/modules/admin/adminLessons.controller.js
@@ -7,41 +7,18 @@
 
     angular
         .module('naut')
-        .controller('ViewStudentsController', ViewStudentsController);
+        .controller('AdminLessonsController', AdminLessonsController);
     
-    ViewStudentsController.$inject = ['$rootScope', '$scope', 'colors', 'flotOptions', '$timeout' ,'$window','StudentInfoService'];
-    function ViewStudentsController($rootScope, $scope, colors, flotOptions, $timeout, $window, StudentInfoService) {
+    AdminLessonsController.$inject = ['$rootScope', '$scope', 'colors', '$timeout' ,'$window','AdminLessonsService','AdminCoursesService'];
+    function AdminLessonsController($rootScope, $scope, colors, $timeout, $window, AdminLessonsService, AdminCoursesService) {
       var vm = this;
-      StudentInfoService.get().success(function(data){
-        $scope.students = data;
+      AdminLessonsService.get().success(function(data){
+        $scope.lessons = data;
       });
 
-      $scope.isAdmin = function(){
-        if ($window.sessionStorage.getItem("userPermissions")=='admin'){
-          return true;
-        } else {
-          return false;
-        }
-      }
-
-      // Some numbers for demo
-      vm.loadProgressValues = function() {
-        vm.progressVal = [0,0,0,0];
-        // helps to show animation when values change
-        $timeout(function(){
-          vm.progressVal[0] = 60;
-          vm.progressVal[1] = 34;
-          vm.progressVal[2] = 22;
-          vm.progressVal[3] = 76;
-        });
-      };
-
-      vm.defaultDate = new Date();
-      vm.toggleDatepicker = function ($event, obj) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        obj.datepickerOpen = !obj.datepickerOpen;
-      }
+      AdminCoursesService.get().success(function(data){
+        $scope.courses = data;
+      });
 
     }
 })();
---
+++ src/js/modules/admin/adminLessons.services.js
@@ -0,0 +1,51 @@
+/**=========================================================
+ * Module: DashboardController.js
+ =========================================================*/
+
+(function() {
+    'use strict';
+
+    angular
+        .module('naut')
+
+    //==============================================
+    // STUDENTS INFO
+    //==============================================
+    .service('AdminLessonsService', ['customUrl', '$http', function(customUrl, $http){
+        var lessons = {};
+        // create a lesson
+        lessons.post = function(data){
+            return $http.post(customUrl.url + '/api/module/', data);
+        };
+        // list of lessons
+        lessons.get = function(){
+            return $http.get(customUrl.url + '/api/module/')
+        };
+        // find 1 lesson
+        lessons.get_single = function(pk){
+            return $http.get(customUrl.url + '/api/module/' + pk + '/');
+        };
+        
+        return lessons;
+    }])
+
+    .service('AdminCoursesService', ['customUrl', '$http', function(customUrl, $http){
+        var courses = {};
+        // create a lesson
+        courses.post = function(data){
+            return $http.post(customUrl.url + '/api/course/', data);
+        };
+        // list of lessons
+        courses.get = function(){
+            return $http.get(customUrl.url + '/api/course/')
+        };
+        // find 1 lesson
+        courses.get_single = function(pk){
+            return $http.get(customUrl.url + '/api/course/' + pk + '/');
+        };
+        
+        return courses;
+    }])
+
+    }
+)();
