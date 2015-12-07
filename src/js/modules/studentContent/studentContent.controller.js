86225a0d69e42539d71c0d942886cc1d60c8d0e3
Ben Havilland <ben@havilland.net>
2015-12-07 12:35:04 -0800

    updated video player

--- src/js/modules/studentContent/studentContent.controller.js
+++ src/js/modules/studentContent/studentContent.controller.js
@@ -49,7 +49,6 @@
       scc.config = {
         autoHide: false,
         autoHideTime: 3000,
-        autoPlay: false,
         //sources: scc.videos[0].sources,
         theme: {
           url: "http://www.videogular.com/styles/themes/default/latest/videogular.css"
@@ -65,9 +64,10 @@
       };
 
       scc.setVideo = function(current_video) {
+        scc.current_video = current_video;
         scc.API.stop();
         scc.API.clearMedia();
-        var tmp_source = [{src: $sce.trustAsResourceUrl($rootScope.url+current_video), type: "video/mp4"}];
+        var tmp_source = [{src: $sce.trustAsResourceUrl($rootScope.url+current_video.file_url), type: "video/mp4"}];
         var tmp_poster = {
           poster: "http://127.0.0.1:3000/app/img/1x1.png"
         };
@@ -75,6 +75,8 @@
         scc.config.plugins = tmp_poster;
 
         scc.config.sources = tmp_source;
+
+        scc.API.play();
 
         //var result = scc.API.changeSource(tmp_source);
       };
