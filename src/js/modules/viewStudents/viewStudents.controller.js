26db316e8659a47e8f1f15b4da4182fd627d59be
Ben Havilland <ben@havilland.net>
2015-12-08 16:25:30 -0800

    admin area photo addition:

--- src/js/modules/viewStudents/viewStudents.controller.js
+++ src/js/modules/viewStudents/viewStudents.controller.js
@@ -14,6 +14,7 @@
       var vm = this;
       StudentInfoService.get().success(function(data){
         $scope.students = data;
+        console.log(data);
       });
 
       $scope.isAdmin = function(){
