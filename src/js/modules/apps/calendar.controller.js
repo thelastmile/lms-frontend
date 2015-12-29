0f2d7c826140aaa28e5e1b9213a09c0e08e69b4c
Ben Havilland <ben@havilland.net>
2015-12-29 12:11:58 -0500

    updated calendar view

--- src/js/modules/apps/calendar.controller.js
+++ src/js/modules/apps/calendar.controller.js
@@ -19,48 +19,48 @@
       var m = date.getMonth();
       var y = date.getFullYear();
 
-      vm.calEventsPers = {
-          id: 0,
-          visible: true,
-          className: ['fc-id-0'],
-          events: [
-            {id: 324, title: 'All Day Event',    start: new Date(y, m, 1) },
-            {         title: 'Long Event',       start: new Date(y, m, d - 5),        end: new Date(y, m, d - 2)},
-            {id: 999, title: 'Repeating Event',  start: new Date(y, m, d - 3, 16, 0),                                     allDay: false},
-            {id: 999, title: 'Repeating Event',  start: new Date(y, m, d + 4, 16, 0),                                     allDay: false},
-            {         title: 'Birthday Party',   start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false},
-            {         title: 'Click for Google', start: new Date(y, m, 28),           end: new Date(y, m, 29),            url: 'http://google.com/'}
-          ]
-        };
+      // vm.calEventsPers = {
+      //     id: 0,
+      //     visible: true,
+      //     className: ['fc-id-0'],
+      //     events: [
+      //       {id: 324, title: 'All Day Event',    start: new Date(y, m, 1) },
+      //       {         title: 'Long Event',       start: new Date(y, m, d - 5),        end: new Date(y, m, d - 2)},
+      //       {id: 999, title: 'Repeating Event',  start: new Date(y, m, d - 3, 16, 0),                                     allDay: false},
+      //       {id: 999, title: 'Repeating Event',  start: new Date(y, m, d + 4, 16, 0),                                     allDay: false},
+      //       {         title: 'Birthday Party',   start: new Date(y, m, d + 1, 19, 0), end: new Date(y, m, d + 1, 22, 30), allDay: false},
+      //       {         title: 'Click for Google', start: new Date(y, m, 28),           end: new Date(y, m, 29),            url: 'http://google.com/'}
+      //     ]
+      //   };
 
-      // event source that pulls from google.com 
-      vm.eventSources = [ vm.calEventsPers];
+      // // event source that pulls from google.com 
+      // vm.eventSources = [ vm.calEventsPers];
 
 
-      $http.get('http://127.0.0.1:3000/server/calendar/external-calendar.json').success(function(data) {
+      // $http.get('http://127.0.0.1:3000/server/calendar/external-calendar.json').success(function(data) {
       
-        var calEventsExt = {
-          id:        2,
-          visible:   true,
-          color:     colors.byName('purple'),
-          textColor: '#fff',
-          className: ['fc-id-2'],
-          events:    []
-        };
+      //   var calEventsExt = {
+      //     id:        2,
+      //     visible:   true,
+      //     color:     colors.byName('purple'),
+      //     textColor: '#fff',
+      //     className: ['fc-id-2'],
+      //     events:    []
+      //   };
       
-        // -----------
-        // override dates just for demo
-        for(var i = 0; i < data.length; i++) {
-            data[i].start = new Date(y, m, d+i, 12, 0);
-            data[i].end   = new Date(y, m, d+i, 14, 0);
-        }
-        // -----------
+      //   // -----------
+      //   // override dates just for demo
+      //   for(var i = 0; i < data.length; i++) {
+      //       data[i].start = new Date(y, m, d+i, 12, 0);
+      //       data[i].end   = new Date(y, m, d+i, 14, 0);
+      //   }
+      //   // -----------
 
-        calEventsExt.events = angular.copy(data);
+      //   calEventsExt.events = angular.copy(data);
 
-        vm.eventSources.push(calEventsExt);
+      //   vm.eventSources.push(calEventsExt);
 
-      });
+      // });
 
       
       /* alert on eventClick */
@@ -80,8 +80,8 @@
         //calendar.fullCalendar('render');
         //calendar.fullCalendar('rerenderEvents');
         //}, 0);
-        $('#my-calendar').fullCalendar('render');
-        $('#my-calendar').fullCalendar('rerenderEvents');
+        $('.calendars').fullCalendar('render');
+        $('.calendars').fullCalendar('rerenderEvents');
       };
 
       // $scope.renderCalendar = function() {
@@ -93,53 +93,175 @@
 
       /* config object */
       vm.uiConfig = {
-        calendar:{
-          height: 450,
-          editable: true,
+        calendar_1:{
+          height: 370,
+          editable: false,
           header:{
-            center: 'title',
+              left:   'false',
+              center: 'title',
+              right:  'false'
           },
-          eventClick:  vm.alertOnEventClick,
-          eventDrop:   vm.alertOnDrop,
-          eventResize: vm.alertOnResize,
-          // Select options
-          selectable: true,
-          selectHelper: true,
+          selectable: false,
+          selectHelper: false,
           unselectAuto: true,
-          select: function(start, end) {
-            var title = prompt('Event Title:');
-            var eventData;
-            if (title) {
-              eventData = {
-                title: title,
-                start: start.format(),
-                end: end.format()
-              };
-              vm.addEvent(eventData);
-            }
-            // vm.myCalendar.fullCalendar( 'unselect' );
+          fixedWeekCount: false,
+          defaultDate: '2015-01-01',
+        },
+        calendar_2:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
           },
-          viewRender: function( view, element ) {
-            touchDrag.addTo(element[0]);
-          }
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-02-01',
+        },
+        calendar_3:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-03-01',
+        },
+        calendar_4:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-04-01',
+        },
+        calendar_5:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-05-01',
+        },
+        calendar_6:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-06-01',
+        },
+        calendar_7:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-07-01',
+        },
+        calendar_8:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-08-01',
+        },
+        calendar_9:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-09-01',
+        },
+        calendar_10:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-10-01',
+        },
+        calendar_11:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-11-01',
+        },
+        calendar_12:{
+          height: 370,
+          editable: false,
+          header:{
+              left:   'false',
+              center: 'title',
+              right:  'false'
+          },
+          selectable: false,
+          selectHelper: false,
+          unselectAuto: true,
+          fixedWeekCount: false,
+          defaultDate: '2015-12-01',
         }
       };
-
-      // Language support
-      // ----------------------------------- 
-      vm.changeTo = 'Español';
-      vm.changeLang = function() {
-        if(vm.changeTo === 'Español'){
-          vm.uiConfig.calendar.dayNames = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-          vm.uiConfig.calendar.dayNamesShort = ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab'];
-          vm.changeTo= 'English';
-        } else {
-          vm.uiConfig.calendar.dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-          vm.uiConfig.calendar.dayNamesShort = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-          vm.changeTo = 'Español';
-        }
-      };
-
     }
     CalendarController.$inject = ['colors', '$http', '$timeout', 'touchDrag'];
 
