c7135226c8ac8d550ea6ce119295810da04afbf7
Ben Havilland <ben@havilland.net>
2016-01-27 17:26:42 -0800

    updated score ui

--- src/js/modules/viewStudents/viewStudents.controller.js
+++ src/js/modules/viewStudents/viewStudents.controller.js
@@ -9,14 +9,20 @@
         .module('naut')
         .controller('ViewStudentsController', ViewStudentsController);
     
-    ViewStudentsController.$inject = ['$rootScope', '$scope', 'colors', 'flotOptions', '$timeout' ,'$window','StudentInfoService', 'toasty' ,'customUrl'];
-    function ViewStudentsController($rootScope, $scope, colors, flotOptions, $timeout, $window, StudentInfoService, toasty, customUrl) {
+    ViewStudentsController.$inject = ['$rootScope', '$scope', 'colors', 'flotOptions', '$timeout' ,'$window','StudentInfoService', 'toasty' ,'customUrl', 'AttendanceService'];
+    function ViewStudentsController($rootScope, $scope, colors, flotOptions, $timeout, $window, StudentInfoService, toasty, customUrl, AttendanceService) {
       var vm = this;
+
+      vm.score_options_participation = [{val:1,label:'1 - Participation'},{val:2,label:'2 - Participation'},{val:3,label:'3 - Participation'},{val:4,label:'4 - Participation'},{val:5,label:'5 - Participation'}]; //used for rating of students
+      vm.score_options_social = [{val:1,label:'1 - Participation'},{val:2,label:'2 - Participation'},{val:3,label:'3 - Participation'},{val:4,label:'4 - Participation'},{val:5,label:'5 - Participation'}]; //used for rating of students
+      vm.score_options_tech = [{val:1,label:'1 - Participation'},{val:2,label:'2 - Participation'},{val:3,label:'3 - Participation'},{val:4,label:'4 - Participation'},{val:5,label:'5 - Participation'}]; //used for rating of students
+
       StudentInfoService.get().success(function(data){
         $scope.students = data;
       });
 
       vm.recordTechnicalScore = function (student) {
+        console.log(student);
         toasty.success({
           title: 'Technical Score Recorded',
           msg: 'The record has been saved.',
@@ -43,21 +49,22 @@
         });
       }
 
-      vm.recordAbsentPart = function (student) {
-        toasty.success({
-          title: 'Attendance Recorded',
-          msg: 'The attendance record has been saved.',
-          sound: false,
-          clickToClose: true
-        });
-      }
-
-      vm.recordAbsentFull = function (student) {
-        toasty.success({
-          title: 'Attendance Recorded',
-          msg: 'The attendance record has been saved.',
-          sound: false,
-          clickToClose: true
+      vm.recordAttendance = function (student,current_attendance) {
+        AttendanceService.put(student,current_attendance).success(function(data){
+          toasty.success({
+            title: 'Attendance Recorded',
+            msg: 'The attendance record has been saved.',
+            sound: false,
+            clickToClose: true
+          });
+        }).error(function (error){
+          console.log('error, not saved');
+          toasty.error({
+            title: 'Attendance Recorded',
+            msg: 'The attendance record not recorded.',
+            sound: false,
+            clickToClose: true
+          });
         });
       }
 
--- src/js/modules/viewStudents/viewStudents.services.js
+++ src/js/modules/viewStudents/viewStudents.services.js
@@ -25,8 +25,28 @@
         students.get_single = function(pk){
             return $http.get(customUrl.url + '/api/students/' + pk + '/');
         };
+
+        // update student
+        students.put = function(data){
+            return $http.put(customUrl.url + '/api/students/'+data.id+'/', data);
+        };
         
         return students;
+    }])
+
+    .service('AttendanceService', ['customUrl', '$http', function(customUrl, $http){
+        var attendance = {};
+        // create a student
+        attendance.post= function(data){
+            return $http.post(customUrl.url + '/api/attendance/', data);
+        };
+
+        // update student
+        attendance.put = function(data,current_attendance){
+            return $http.put(customUrl.url + '/api/attendance/'+data.attendance_id+'/', {attendance: current_attendance, student:data.id});
+        };
+        
+        return attendance;
     }])
 
     }
